592b2533b11e1b73975a91853e25bc3abf8497c8
Rian Husen <rianhusen0@gmail.com>
2022-08-09 15:53:28 +0700

    Flutter hari ke 2

---
+++ lib/main.dart
@@ -0,0 +1,229 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /*3*/
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text('5541'),
+        ],
+      ),
+    );
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
+    Widget textSection = const Padding(
+      padding: EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            //const Image(
+            //  image: NetworkImage(
+            //    'https://raw.githubusercontent.com/flutter/website/main/examples/layout/lakes/interactive/images/lake.jpg',
+            //  ),
+            //),
+
+            // Image.asset('Images/cool.jpg'),
+            Image.network(
+                'https://raw.githubusercontent.com/flutter/website/main/examples/layout/lakes/interactive/images/lake.jpg'),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 16.0,
+                top: 16.0,
+                right: 16.0,
+                bottom: 8.0,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Column(
+                    children: const <Widget>[
+                      Text(
+                        'Oeschinen Lake Campground',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text('Kandersteg, Switzerland'),
+                    ],
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const <Widget>[
+                      Icon(Icons.star),
+                      Text('41'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+/** Icons */
+            Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: 8.0,
+                horizontal: 16.0,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  CustomButton(
+                    iconData: Icons.call,
+                    text: 'CALL',
+                  ),
+                  CustomButton(
+                    iconData: Icons.near_me,
+                    text: 'ROUTE',
+                  ),
+                  CustomButton(
+                    iconData: Icons.share,
+                    text: 'SHARE',
+                  ),
+                ],
+              ),
+            ),
+
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
+        ),
+      ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class CustomButton extends StatefulWidget {
+  CustomButton({
+    Key? key,
+    required this.iconData,
+    required this.text,
+  }) : super(key: key);
+
+  final IconData iconData;
+  final String text;
+
+  @override
+  State<CustomButton> createState() => _CustomButtonState();
+}
+
+class _CustomButtonState extends State<CustomButton> {
+  bool _isOn = false;
+
+  @override
+  Widget build(BuildContext context) {
+    debugPrint('${DateTime.now()} - onBuild - ${widget.text} - $_isOn');
+    return InkWell(
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            Icon(
+              widget.iconData,
+              color: _isOn ? Colors.red : Colors.blue,
+            ),
+            Text(widget.text),
+          ],
+        ),
+      ),
+      onTap: () {
+        _isOn = !_isOn;
+        setState(() {});
+      },
+    );
+  }
+}
